dacfda7ce126911a7aee02d2ccec8ce53868a984
lwylie99 <76063565+lwylie99@users.noreply.github.com>
2023-03-25 12:46:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import arcade
 import random
 
-from traffic_sim import TrafficSim
+from traffic_sim import Game
 
 
 def main():
     # WINDOW_WIDTH = 800
     # WINDOW_HEIGHT = 600
 
-    window = TrafficSim(800, 600, "Traffic Simulator")
+    window = Game(800, 600, "Traffic Simulator")
     arcade.run()
 
 if __name__ == "__main__":
